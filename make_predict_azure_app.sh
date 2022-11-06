be70017180f15bb73e64cb6206e7479247e3382a
sinle <sinle@DESKTOP-RV9FV71>
2022-11-06 11:04:30 +0700

    update .sh file

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://udacity-sinlq3-project2.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
