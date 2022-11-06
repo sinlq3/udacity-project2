8e587e193a98ffc955ebac2b3b616dad1958a4ed
sinle <sinle@DESKTOP-RV9FV71>
2022-11-06 16:20:03 +0700

    Change the version to 1.0

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home version 1.0</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
